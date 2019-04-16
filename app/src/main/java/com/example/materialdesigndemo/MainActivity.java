828915068a7016c78692d96afb5e06eba7bce9f7
Tankmi <1103334665>
2019-04-16 16:10:44 +0800

    初始化了首页的toolbar

--- app/src/main/java/com/example/materialdesigndemo/MainActivity.java
+++ app/src/main/java/com/example/materialdesigndemo/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.materialdesigndemo;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mtoolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mtoolBar = findViewById(R.id.toolbar_title);
+        setSupportActionBar(mtoolBar);
+
+        //获取actionbar对象，也就是这里的toolbar，设置左侧按钮的图标（默认是返回键）
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+//            actionBar.setHomeAsUpIndicator(R.drawable.menu_icon);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu:
+
+                break;
+            case android.R.id.home:
+                Log.i("test","加载菜单");
+                break;
+        }
+        return true;
     }
 }
